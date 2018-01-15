84a10f1db3f721d97041d1e89386468c7a6672a4
jburto94 <jburto94@yahoo.com>
2018-01-14 23:02:09 -0500

    Added blog and pricing section

--- template-3/assets/scripts/script.js
+++ template-3/assets/scripts/script.js
@@ -1,9 +1,29 @@
-function scrollFunction() {
-	if(document.documentElement.scrollTop > 500) {
-		document.querySelector('nav').style.top = "0";
+function navScroll() {
+	var nav = document.querySelector('nav');
+	var scroll = window.pageYOffset;
+	var hidden = "-" + nav.offsetHeight + "px";
+	if (scroll > 500) {
+		nav.style.top = "0";
 	} else {
-		document.querySelector('nav').style.top = "-100px";
+		nav.style.top = hidden.toString();
 	}
 }
 
-window.addEventListener('scroll', scrollFunction);
+window.addEventListener('scroll', navScroll);
+
+//Scrolls the blog backgrounds when hovered
+var blogs = document.getElementsByClassName('blog');
+
+function blogScroll(e) {
+	this.style.backgroundPosition = "center bottom";
+}
+
+function blogReset(e) {
+	this.style.backgroundPosition = "center top";
+}
+
+
+for (var i = 0; i < blogs.length; i++) {
+	blogs[i].addEventListener('mouseover', blogScroll);
+	blogs[i].addEventListener('mouseout', blogReset);
+}
